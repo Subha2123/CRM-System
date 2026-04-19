962d326019ae20ed6df18a2b9ca259fe674a4f8d
Subha2123 <subhashinirbca@gmail.com>
2026-04-19 15:08:35 +0530

    Cors fixed

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://crm-system-jkuo.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://crm-system-jkuo.vercel.app'],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
