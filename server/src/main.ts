24324521417ba553347246b378bf3dd7033ff9b3
Subha2123 <subhashinirbca@gmail.com>
2026-04-19 15:03:50 +0530

    cloud URL added for cors policy

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true, // if you're using cookies/auth headers
+    origin: ['http://localhost:3000', 'https://crm-system-jkuo.vercel.app/'],
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
